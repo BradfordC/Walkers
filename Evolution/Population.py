0e88b5ec4e0222b153e79727840463c4d3181c4e
BradfordC <chjabr@gmail.com>
2017-05-26 17:05:59 -0500

    Add basic generations

--- Evolution/Population.py
+++ Evolution/Population.py
@@ -3,12 +3,22 @@ from Evolution import Agent
 
 class Population:
     def __init__(self, size, baseNetwork):
-        self.AgentList = []
+        self.agentList = []
 
         layerSizes = baseNetwork.GetLayerSizes()
 
         for i in range(size):
             nextNetwork = Network.Network(layerSizes[0], layerSizes[-1], layerSizes[1:-1])
             nextAgent = Agent.Agent(nextNetwork)
-            self.AgentList.append(nextAgent)
+            self.agentList.append(nextAgent)
+
+    def makeNextPopulation(self):
+        return self
+
+    def getHighestFitness(self):
+        highestFitness = -999999999
+        for agent in self.agentList:
+            if(highestFitness < agent.fitness):
+                highestFitness = agent.fitness
+        return highestFitness
 
--- Simulator.py
+++ Simulator.py
@@ -2,7 +2,8 @@ from .framework import (Framework, main)
 from math import cos
 from Box2D import *
 from Walker import Walker
-from Networks import Network
+from Networks.Network import Network
+from Evolution.Population import Population
 
 
 class Simulator(Framework):
@@ -10,30 +11,42 @@ class Simulator(Framework):
 
     def __init__(self):
         super(Simulator, self).__init__()
-
+        #Create the ground
         self.world.CreateStaticBody(position=(0, -10), shapes=b2PolygonShape(box=(50, 10)))
 
-        self.walker = Walker(self.world)
         self.secondsPerTrial = 5
+        self.walkerCount = 5
 
-        jointCount = len(self.walker.jointList)
-        self.network = Network.Network(jointCount, jointCount, [jointCount])
+        #Make some walkers
+        self.walkerList = []
+        for i in range(self.walkerCount):
+            self.walkerList.append(Walker(self.world))
 
-
+        #Make a population of agents
+        jointCount = len(self.walkerList[0].jointList)
+        sampleNetwork = Network(jointCount, jointCount, [jointCount])
+        self.population = Population(self.walkerCount, sampleNetwork)
 
     def Step(self, settings):
         super(Simulator, self).Step(settings)
 
-        output = self.network.Feedforwad(self.walker.getJointAngles())
-        forces = [(i - .5)*10 for i in output]
-        self.walker.setJointForces(forces)
+        #Advance all walkers
+        for i in range(self.walkerCount):
+            walker = self.walkerList[i]
+            network = self.population.agentList[i].network
+            networkOutput = network.Feedforwad(walker.getJointAngles())
+            jointForces = [(i - .5)*10 for i in networkOutput]
+            walker.setJointForces(jointForces)
 
+        #Deal with the end of a trial
         if (self.stepCount % (self.secondsPerTrial * 60)) == 0:
-            print(self.walker.getTorsoPosition())
-            self.walker.resetPosition()
-            jointCount = len(self.walker.jointList)
-            self.network = Network.Network(jointCount, jointCount, [jointCount])
-            print(forces)
+            for i in range(self.walkerCount):
+                walker = self.walkerList[i]
+                self.population.agentList[i].fitness = walker.getTorsoPosition;
+                walker.resetPosition()
+
+            self.population = self.population.makeNextPopulation()
+
 
 
 if __name__ == "__main__":
